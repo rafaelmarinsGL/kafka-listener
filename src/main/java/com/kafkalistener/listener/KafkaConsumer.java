f7b13a3ccf05d81ee6a09e9a4e32ca55250dc7e0
rafaelCarinha <rafael.marins@gorillalogic.com>
2019-02-19 17:22:28 -0600

    Initial Commit

---
+++ src/main/java/com/kafkalistener/listener/KafkaConsumer.java
@@ -0,0 +1,21 @@
+package com.kafkalistener.listener;
+
+import com.kafkalistener.model.User;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "Kafka_Example")
+    public void consume(String message) {
+        System.out.println("Consumed message: " + message);
+    }
+
+
+    @KafkaListener(topics = "Kafka_Example_json",
+            containerFactory = "userKafkaListenerFactory")
+    public void consumeJson(User user) {
+        System.out.println("Consumed JSON Message: " + user);
+    }
+}
---
+++ src/main/java/com/kafkalistener/model/User.java
@@ -0,0 +1,14 @@
+package com.kafkalistener.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class User {
+
+    private String name;
+    private String dept;
+}
